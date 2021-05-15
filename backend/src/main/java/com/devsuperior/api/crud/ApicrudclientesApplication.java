8a71b38147334cd8be68b424f40aacb99c472981
Murilo Carnelos <muriloc.c@hotmail.com>
2021-05-15 16:14:24 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/api/crud/ApicrudclientesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.api.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicrudclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicrudclientesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/api/crud/ApicrudclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.api.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicrudclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
